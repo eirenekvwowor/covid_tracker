5271f66f3ae47cbf121f5c1e1266b7535b5fe4ab
eirenekvwowor <eirene.wowor@gmail.com>
2020-12-08 12:27:24 +0700

    comit untuk tahap indojs

---
+++ covid19app/src/pages/Indonesia.js
@@ -0,0 +1,52 @@
+
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import CountUp from "react-countup";
+
+const Indonesia = () =>{
+    const[jum, setJum] = useState([])
+    const[sembuh, setSembuh] = useState([])
+    const[men, setMen] = useState([])
+
+    useEffect(() => {
+        axios
+         .get("https://indonesia-covid-19.mathdro.id/api")
+         .then((response) => {
+            setJum(response.data.jumlahKasus)
+            setSembuh(response.data.sembuh)
+            setMen(response.data.meninggal)
+        });
+    }, []);
+
+            return (
+                <div className="center">
+        
+        <div className="card-containerr">
+             <h1>Positif
+                <p><CountUp start={0} end={jum} delay={1.5} separator=",">
+                     {({ countUpRef }) => (<span ref={countUpRef} />)} 
+                     </CountUp> </p></h1>
+                     </div>
+
+            <div className="card2-container">
+             <h1>Sembuh
+                <p><CountUp start={0} end={sembuh} delay={1.5} separator=",">
+                     {({ countUpRef }) => (<span ref={countUpRef} />)} 
+                     </CountUp> </p></h1>
+                     </div>
+
+                     <div className="card1-container">
+             <h1>Meninggal
+                <p><CountUp start={0} end={men} delay={1.5} separator=",">
+                     {({ countUpRef }) => (<span ref={countUpRef} />)} 
+                     </CountUp> </p></h1>
+                     </div>
+     </div>
+
+    
+            );
+            
+};
+
+export default Indonesia;
+
